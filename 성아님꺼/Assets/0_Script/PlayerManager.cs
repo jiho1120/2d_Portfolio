54ebbf4e9709947aa7333ba9b461c1da1f3ada2a
sungya <saj.qsrsr.186@gmail.com>
2023-11-23 12:16:46 +0900

    성아님꺼

---
+++ 성아님꺼/Assets/0_Script/PlayerManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerManager : MonoBehaviour
+{
+    public Player player;
+    public PlayerSkill skill;
+
+    #region ΩÃ±€≈Ê
+    private static PlayerManager instance = null;
+    public static PlayerManager Instance => instance;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+    #endregion ΩÃ±€≈Ê
+}
